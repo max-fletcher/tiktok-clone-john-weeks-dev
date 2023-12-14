fe226f60607bec5b3bb756a099c42897d4314445
max-fletcher <amar.naam.machine@gmail.com>
2023-12-14 14:27:47 +0600

    Done upto 29:10.

---
+++ components/PostMain.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="PostMain" class="flex border-b py-6">
+    <div class="cursor-pointer">
+      <img class="rounded-full max-h-[60px]" width="60" src="https://picsum.photos/id/8/300/320" />
+    </div>
+    <div class="pl-3 w-full px-4">
+      <div class="flex items-center justify-between pb-0.5">
+        <button>
+          <span class="font-bold hover:underline cursor-pointer">
+            User Name
+          </span>
+          <span class="text-[13px] text-light text-gray-500 pl-1 cursor-pointer">
+            User Name
+          </span>
+        </button>
+
+        <button class="border text-[15px] px-[21px] py-0.5 border-[#F02C56] text-[#F02C56] hover:bg-[#ffeef2] font-semibold rounded-md">
+          Follow
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
     <!-- ANOTHER WAY TO DEFINE LAYOUT WITH IMPORT -->
-    <!-- <MainLayout /> -->
-  </div>
+    <div class="pt-[80px] w-[calc(100%-90px)] max-w-[690px]">
+      <PostMain />
+    </div>
 </template>
 
 <script setup lang="ts">
-  // import MainLayout from '~/layouts/MainLayout.vue'; // ANOTHER WAY TO DEFINE LAYOUT WITH IMPORT
+// import MainLayout from '~/layouts/MainLayout.vue'; // ANOTHER WAY TO DEFINE LAYOUT WITH IMPORT
 
   definePageMeta({
     // NOTICE THAT THE LAYOUT COMPONENT IS NAMED MainLayout BUT HERE WE ARE CALLING IT main-layout. THIS IS BECAUSE WE CAN'T USE UNDERSCORES AND CAPITALS HERE AND ANY CAMELCASE IS PARSED AS 
