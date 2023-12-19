034c2da365536aa1f14afd0246fd66e3f6ed5242
max-fletcher <amar.naam.machine@gmail.com>
2023-12-19 11:21:50 +0600

    Done upto 1:02:03.

---
+++ components/PostUser.vue
@@ -0,0 +1,78 @@
+<template>
+  <div 
+    @mouseenter="isHover(true)"
+    @mouseleave="isHover(false)"
+    class="relative brightness-90 hover:brightness-[1.1] cursor-pointer"
+  >
+    <div 
+      v-if="!isLoaded"
+      class="absolute flex items-center justify-center top-0 left-0 aspect-[3/4] w-full object-cover rounded-md bg-black"
+    >
+      <Icon class="animate-spin ml-1" name="mingcute:loading-line" size="100" color="#FFFFFF" />
+    </div>
+
+    <div>
+      <video
+        ref="video"
+        muted
+        loop
+        class="aspect-[3/4] object-cover rounded-md"
+        src="/cowe.mp4"
+      />
+    </div>
+
+    <div class="px-1">
+      <div class="text-gray-700 text-[15px] pt-1 break-words">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      </div>
+      <div class="flex items-center -ml-1 text-gray-600 font-bold text-xs">
+        <Icon name="gg:loadbar-sound" size="20" />
+        3%
+        <Icon class="ml-1" name="tabler:alert-circle" size="16" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineProps(['post'])
+
+  const route = useRoute()
+  const router = useRouter()
+
+  let video = ref(null)
+  let isLoaded = ref(false)
+
+  onMounted(() => {
+    // IF THE VIDEO VARIABLE IS NOT EMPTY, APPEND AN EVENT LISTENER TO THE VIDEO VARIABLE 'video'. THE TERM 'loadeddata' IS A CASE-SENSITIVE RESERVED KEYWORD THAT TELLS THE
+    // EVENT-LISTENER TO DO SOMETHING IN RESPONSE TO WHEN DATA IS FINISHED LOADING FOR, SAY, A VIDEO TAG.
+    if(video.value) {
+      video.value.addEventListener('loadeddata', (e) => {
+        if(e.target){
+          // SET 'isLoaded' VALUE TO true AFTER 200ms OF WHEN DATA IS FINISHED LOADING(I.E FINISHED LOADING IN VIDEO TAG)
+          setTimeout(() => {
+            isLoaded.value = true
+          }, 200)
+        }
+      })
+    }
+  })
+
+  console.log(toRaw(video.value), toRaw(isLoaded.value));
+
+  // WILL PAUSE THE VIDEO, REWIND THE VIDEO BACK TO TIME=0 AND SET VIDEO SRC TO EMPTY STRING(I.E STOP THE VIDEO)
+  onBeforeUnmount(() => {
+    video.value.pause()
+    video.value.currentTime = 0
+    video.value.src = ''
+  })
+
+  const isHover = (bool) => {
+    if(bool){
+      video.value.play()
+    }
+    else{
+      video.value.pause()
+    }
+  }
+</script>
---
+++ pages/profile/[id].vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="pt-[90px] 2xl:pl-[185px] lg:pl-[160px] lg:pr-0 pr-2 w-[calc(100%-90px)] max-w-[1800px] 2xl:mx-auto">
+    <div class="flex w-[calc(100vw-230px)]">
+      <img src="https://picsum.photos/id/8/300/320" class="max-w-[120px] rounded-full" />
+      <div class="ml-5 w-full">
+        <div class="text-[30px] font-bold truncate">
+          User Name
+        </div>
+        <div class="text-[18px] truncate">User Name</div>
+        <button v-if="true" class="flex items-center rounded-md py-1.5 px-3.5 text-[15px] font-semibold border hover:bg-gray-100">
+          <Icon class="mt-0.5 mr-1" name="mdi:pencil" size="18" />
+          Edit Profile
+        </button>
+        <button v-else class="flex items-center rounded-md py-1.5 px-8 mt-3 text-[15px] text-white font-semibold bg-[#F02C56]">
+          Following
+        </button>
+      </div>
+    </div>
+    <div class="flex items-center pt-4">
+      <div class="mr-4">
+        <span class="font-bold">10K</span>
+        <span class="text-gray-500 font-light text-[15px] pl-1.5">Following</span>
+      </div>
+      <div class="mr-4">
+        <span class="font-bold">33K</span>
+        <span class="text-gray-500 font-light text-[15px] pl-1.5">Followers</span>
+      </div>
+      <div class="mr-4">
+        <span class="font-bold">3K</span>
+        <span class="text-gray-500 font-light text-[15px] pl-1.5">Likes</span>
+      </div>
+    </div>
+
+    <div class="pt-4 mr-4 text-gray-500 font-light text-[15px] pl-1.5 max-w-[500px]">
+      This is the bio section
+    </div>
+    <div class="w-full flex items-center pt-4 border-b">
+      <div class="w-60 text-center py-2 text-[17px] font-semibold border-b-2 border-b-black">Videos</div>
+      <div class="w-60 text-gray-500 text-center py-2 text-[17px] font-semibold">
+        <Icon name="material-symbols:lock-open" class="mb-0.5" />
+        Liked
+      </div>
+    </div>
+    <div class="mt-4 grid 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-3">
+      <PostUser />
+      <PostUser />
+      <PostUser />
+      <PostUser />
+      <PostUser />
+      <PostUser />
+      <PostUser />
+      <PostUser />
+      <PostUser />
+      <PostUser />
+      <PostUser />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// import MainLayout from '~/layouts/MainLayout.vue'; // ANOTHER WAY TO DEFINE LAYOUT WITH IMPORT
+
+  definePageMeta({
+    // NOTICE THAT THE LAYOUT COMPONENT IS NAMED MainLayout BUT HERE WE ARE CALLING IT main-layout. THIS IS BECAUSE WE CAN'T USE UNDERSCORES AND CAPITALS HERE AND ANY CAMELCASE IS PARSED AS 
+    // KEBAB-CASE BY NUXT FOR DEFINE-COMPOSABLES
+    layout: 'main-layout',
+  })
+</script>
