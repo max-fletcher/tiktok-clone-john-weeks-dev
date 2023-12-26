cef775186318776480650f023767e1a512ff538e
max-fletcher <amar.naam.machine@gmail.com>
2023-12-26 18:05:07 +0600

    Done upto 2:18:00.

--- client/app.vue
+++ client/app.vue
@@ -9,11 +9,35 @@
 </template>
 
 <script setup lang="ts">
-  // 'storeToRefs' CONVERTS STORE STATES AND GETTERS TO REFS SO IT CAN BE DESTRUCTURED WHILE MAINTAINING REACTIVITY, AND IF NEED BE, PASSED AS PROP TO ANY COMPONENT. 
+  // 'storeToRefs' CONVERTS STORE STATES AND GETTERS TO REFS SO IT CAN BE DESTRUCTURED WHILE MAINTAINING REACTIVITY, AND IF NEED BE, PASSED AS PROP TO ANY COMPONENT.
   // ACTIONS CAN BE DESTRUCTURED DIRECTLY WITHOUT USING 'storeToRefs'.
   import { storeToRefs } from 'pinia'
 
   // IMPORT '$generalStore' FROM useNuxtApp(ALIASED INSIDE plugins/store) AND GET THE 'isLoginOpen' STATE AS A REF FROM IT
-  const { $generalStore } = useNuxtApp()
+  const { $userStore, $generalStore } = useNuxtApp()
   const { isLoginOpen, isEditProfileOpen } = storeToRefs($generalStore)
+
+  onMounted(async () => {
+    // THIS IS TO PREVENT SCROLLING WHEN MODALS(LOGIN/REGISTER AND EDITPROFILE) ARE OPEN. ALSO SEE 'general.js' - 'bodySwitch(val)' (WHERE 'generalStore' ACTIONS ARE DEFINED) FOR THE REST OF THE FUNCTIONAL CODE
+    $generalStore.bodySwitch(false)
+    isLoginOpen.value = false
+    isEditProfileOpen.value = false
+
+  // WORKS LIKE A MIDDLEWARE. WHENEVER A PAGE IS LOADED, THIS CHECKS IF SESSION HAS EXPIRED OR NOT USING INTERCEPTORS WHENEVER A REQUEST TO THE SERVER(USING AXIOS) IS MADE
+    try {
+      await $generalStore.hasSessionExpired()
+
+      if($userStore.id){
+        $userStore.getUser()
+      }
+
+    } catch (error) {
+      console.log(error);
+    }
+  })
+
+  // WHEN isLoginOpen.value OR isEditProfileOpen.value CHANGES, THEIR VALUE(i.e val) IS FED INTO THE 'bodyswitch' FUNCTION IN $generalStore, WHICH CAUSES SCROLLING TO BE DISABLED
+  // SEE ABOVE BLOCK AND 'general.js' - 'bodySwitch(val)' (WHERE 'generalStore' ACTIONS ARE DEFINED) FOR THE REST OF THE FUNCTIONAL CODE
+  watch(() => isLoginOpen.value, (val) => $generalStore.bodySwitch(val))
+  watch(() => isEditProfileOpen.value, (val) => $generalStore.bodySwitch(val))
 </script>
--- client/stores/general.js
+++ client/stores/general.js
@@ -1,9 +1,13 @@
 import { defineStore } from 'pinia'
+import { useUsersStore } from './user'
+import axios from '../plugins/axios' // IMPORT AXIOS INSTANCE FROM AXIOS.JS.
+
+const $axios = axios().provide.axios //GETTING THE AXIOS INSTANCE PROVIDED BY PROVIDER. SEE THAT FILE.
 
 // THE <-- persist: true --> IS USING NUXT_PINIA_PERSIST PACKAGE TO STORE THESE STORES AS COOKIES
 export const useGeneralStore = defineStore('general', {
   state: () => ({
-    isLoginOpen : '',
+    isLoginOpen : false,
     isEditProfileOpen : false,
     selectedPost : '',
     ids : '',
@@ -13,7 +17,37 @@ export const useGeneralStore = defineStore('general', {
     following: '',
   }),
   actions: {
-    //
+    // THIS IS TO PREVENT SCROLLING WHEN THE MODALS ARE OPEN. ALSO SEE app.vue onMounted FOR THE REST OF THE FUNCTIONAL CODE
+    bodySwitch(val){
+      if(val){
+        document.body.style.overflow = 'hidden'
+        return
+      }
+      document.body.style.overflow = 'visible'
+    },
+
+    async hasSessionExpired(){
+      await $axios.interceptors.response.use((response) => {
+        // CALL WAS SUCCESSFUL
+        return response;
+      }, (error) => {
+        // A SWITCH CASE THAT USES THE ERROR RESPONSE STATUS CODE
+        switch(error.response.status){
+          case 401:
+          case 419:
+          case 503:
+            useUsersStore().resetUser()
+            window.location.href = "/"
+            break
+          case 500:
+            alert('Oops! Something went wrong! The team has been notified.')
+            break
+          default:
+            // ALLOW INDIVIDUAL REQUESTS TO HANDLE OTHER ERRORS(I.E 422 AND 401)
+            return Promise.reject(error);
+        }
+      })
+    }
   },
   persist: true,
 })
